0b9aaba8e478a1bee7a2d0ed0ef80fcc3e5e6d23
hedi02 <hedi02@gmail.com>
2023-04-17 10:51:41 +0700

    update sshNgrok.sh

--- sshNgrok.sh
+++ sshNgrok.sh
@@ -1,3 +1,4 @@
+#
 result=$(curl -s -X GET -H "Authorization: Bearer API_KEY" -H "Ngrok-Version: 2" https://api.ngrok.com/tunnels | jq -r '.tunnels[].public_url')
 url=$(echo $result | sed 's#^tcp://##;s/:.*$//')
 port=$(echo $result | sed 's/.*://')
